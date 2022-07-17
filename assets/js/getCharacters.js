75c8624428beaab0a5806bd6f98c9063a138b22d
JhonE17 <40145138+JhonE17@users.noreply.github.com>
2022-07-17 14:43:53 -0500

    Update getCharacters.js

--- assets/js/getCharacters.js
+++ assets/js/getCharacters.js
@@ -2,7 +2,7 @@ const URL = "https://rickandmortyapi.com/api/character"
 const getAllAvatars = async (url) => {
     try {
         const res = await axios(url)
-        const data = await res.data.results
+        const data = await res.data
         return data
     } catch (error) {
         console.log(error);
